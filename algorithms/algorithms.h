65af46887965f44178fb2e7b8398d0a1592c5035
Ícaro Pereira <46682140+icarosun@users.noreply.github.com>
2024-07-06 12:30:53 +0000

    doc: add comments

--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -3,7 +3,7 @@
 
 #include <bits/stdc++.h>
 
-void print_vetor(int array[], int sizearray); 
+void print_vetor(int array[], int sizearray); //func aux
 void insertion_sort(int array[], int sizearray); //exercise 2.1-2
 int linear_search(int array[], int sizearray, int key); //exercise 2.1-3
 void plus_bit(int parcela_a[], int parcela_b[]); //plus num bit with 2 cases

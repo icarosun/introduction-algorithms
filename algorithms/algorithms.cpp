7e1d6608f5b751d20f5d4ad091fd98d2491d3699
Sun Stark <icaropereirasun@gmail.com>
2025-11-01 16:04:07 -0400

    Format lsp

--- algorithms/algorithms.cpp
+++ algorithms/algorithms.cpp
@@ -4,271 +4,272 @@
 using namespace std;
 
 void print_vetor(int array[], int sizearray) {
-    int i;
+  int i;
 
-    for (i = 0; i < sizearray; i++) {
-        cout << array[i] << " ";
-    }
-    
-    cout << endl;
+  for (i = 0; i < sizearray; i++) {
+    cout << array[i] << " ";
+  }
+
+  cout << endl;
 }
 
 void insertion_sort(int array[], int sizearray) {
-    int key = 0, j, i; 
+  int key = 0, j, i;
 
-    for (j = 1; j < sizearray; j++) {
-        key = array[j];
-        i = j - 1;
-        while (i > -1 && array[i] > key) {
-            array[i + 1] = array[i];
-            i--;
-        }
-        array[i + 1] = key;
+  for (j = 1; j < sizearray; j++) {
+    key = array[j];
+    i = j - 1;
+    while (i > -1 && array[i] > key) {
+      array[i + 1] = array[i];
+      i--;
     }
+    array[i + 1] = key;
+  }
 }
 
 int linear_search(int array[], int sizearray, int key) {
-    int i;
+  int i;
 
-    for (i = 0; i < sizearray; i++) {
-        if (array[i] == key) {
-            return i;
-        }
+  for (i = 0; i < sizearray; i++) {
+    if (array[i] == key) {
+      return i;
     }
+  }
 
-    return -1;
+  return -1;
 }
 
 void plus_bit(int operator_a[], int operator_b[]) {
-    int result[] = {0, 0, 0}; 
-    int i, sum = 0, chunk = 0;
+  int result[] = {0, 0, 0};
+  int i, sum = 0, chunk = 0;
 
-    for (i = 1; i > -1; i--) {
-        sum = operator_a[i] + operator_b[i] + chunk;
+  for (i = 1; i > -1; i--) {
+    sum = operator_a[i] + operator_b[i] + chunk;
 
-        if (sum >= 2) {
-            chunk = 1;
-            result[i + 1] = sum % 2;      
-        } else {
-            chunk = 0;
-            result[i + 1] = sum;
-        }
+    if (sum >= 2) {
+      chunk = 1;
+      result[i + 1] = sum % 2;
+    } else {
+      chunk = 0;
+      result[i + 1] = sum;
     }
+  }
 
-    result[0] = chunk;
+  result[0] = chunk;
 
-    for (i = 0; i < 3; i++) {
-        cout << result[i] << endl;
-    }
+  for (i = 0; i < 3; i++) {
+    cout << result[i] << endl;
+  }
 }
 
 void insertion_sort_with_search_minus_value(int array[], int sizearray) {
-    int minus_indice = 0, i, j, aux; 
+  int minus_indice = 0, i, j, aux;
 
-    for (i = 0; i < sizearray - 1; i++) {
-        minus_indice = i;
-        for (j = i + 1; j < sizearray; j++) {
-            if (array[j] < array[minus_indice]) {
-                minus_indice = j;
-            }
-        }
-
-        aux = array[i];
-        array[i] = array[minus_indice];
-        array[minus_indice] = aux;
+  for (i = 0; i < sizearray - 1; i++) {
+    minus_indice = i;
+    for (j = i + 1; j < sizearray; j++) {
+      if (array[j] < array[minus_indice]) {
+        minus_indice = j;
+      }
     }
+
+    aux = array[i];
+    array[i] = array[minus_indice];
+    array[minus_indice] = aux;
+  }
 }
 
-void mescla(int array[], int init_left, int tail_left, int init_right, int tail_right) {
-    int indice_array = init_left, aux, i = 0, j = 0, k; 
-    int left[tail_left-init_left], right[tail_right - init_right];
-    int sizevetor;
-    
-    for (aux = init_left; aux < tail_left; aux++) {
-        left[i] = array[aux];
-        i++;
-    }
+void mescla(int array[], int init_left, int tail_left, int init_right,
+            int tail_right) {
+  int indice_array = init_left, aux, i = 0, j = 0, k;
+  int left[tail_left - init_left], right[tail_right - init_right];
+  int sizevetor;
 
-    for (aux = init_right; aux < tail_right; aux++) {
-        right[j] = array[aux];
-        j++; 
-    }
+  for (aux = init_left; aux < tail_left; aux++) {
+    left[i] = array[aux];
+    i++;
+  }
 
-    i = 0;
-    j = 0;
-    k = init_left; 
-    
-    while (init_left < tail_left && init_right < tail_right) {
-        if (left[i] < right[j]) {
-            array[k] = left[i];
-            i++;
-            init_left++;
-        } else {
-            array[k] = right[j];
-            init_right++;
-            j++;
-        }
-        k++;
-    }    
+  for (aux = init_right; aux < tail_right; aux++) {
+    right[j] = array[aux];
+    j++;
+  }
 
-    sizevetor = sizeof(left)/sizeof(left[0]);
-    for (i; i < sizevetor; i++) {
-        array[k] = left[i];
-        k++;
-    }
+  i = 0;
+  j = 0;
+  k = init_left;
 
-    sizevetor = sizeof(right)/sizeof(right[0]);
-    for (j; j < sizevetor; j++) {
-        array[k] = right[j];
-        k++;
+  while (init_left < tail_left && init_right < tail_right) {
+    if (left[i] < right[j]) {
+      array[k] = left[i];
+      i++;
+      init_left++;
+    } else {
+      array[k] = right[j];
+      init_right++;
+      j++;
     }
+    k++;
+  }
+
+  sizevetor = sizeof(left) / sizeof(left[0]);
+  for (i; i < sizevetor; i++) {
+    array[k] = left[i];
+    k++;
+  }
+
+  sizevetor = sizeof(right) / sizeof(right[0]);
+  for (j; j < sizevetor; j++) {
+    array[k] = right[j];
+    k++;
+  }
 }
 
-void merge_sort(int array[], int init_array, int sizearray) { 
-    int left_array[sizearray], right_array[sizearray], init_left = 0, init_right = 0;
-    int tail_left_array = 0, tail_right_array = 0;
+void merge_sort(int array[], int init_array, int sizearray) {
+  int left_array[sizearray], right_array[sizearray], init_left = 0,
+                                                     init_right = 0;
+  int tail_left_array = 0, tail_right_array = 0;
 
-    int medium = (init_array + sizearray) / 2; 
+  int medium = (init_array + sizearray) / 2;
 
-    if (medium > init_array) {
-        merge_sort(array, init_array, medium);
-        merge_sort(array, medium, sizearray);
-        mescla(array, init_array, medium, medium, sizearray);
-    }
+  if (medium > init_array) {
+    merge_sort(array, init_array, medium);
+    merge_sort(array, medium, sizearray);
+    mescla(array, init_array, medium, medium, sizearray);
+  }
 }
 
 void insertion_sort_recursive(int array[], int sizearray) {
-    int key, i;
+  int key, i;
 
-    if (sizearray <= 1) {
-        return;
-    }
-    
-    insertion_sort_recursive(array, sizearray - 1);
+  if (sizearray <= 1) {
+    return;
+  }
 
-    key = array[sizearray - 1];
+  insertion_sort_recursive(array, sizearray - 1);
 
-    i = sizearray - 2;
-    while (i > -1 && array[i] > key) {
-        array[i + 1] = array[i];
-        i--;
-    }
-    
-    array[i + 1] = key;
+  key = array[sizearray - 1];
+
+  i = sizearray - 2;
+  while (i > -1 && array[i] > key) {
+    array[i + 1] = array[i];
+    i--;
+  }
+
+  array[i + 1] = key;
 }
 
 int binary_search(int array[], int left, int right, int key) {
-    int mid;
+  int mid;
 
-    while (left <= right) {
-        mid = (left + right) / 2;
+  while (left <= right) {
+    mid = (left + right) / 2;
 
-        if (array[mid] == key) {
-            return mid;
-        }
-
-        if (array[mid] < key) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+    if (array[mid] == key) {
+      return mid;
     }
 
-    return -1;
+    if (array[mid] < key) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
 int binary_search_recursive(int array[], int left, int right, int key) {
-    int mid = (left + right) / 2;
+  int mid = (left + right) / 2;
 
-    if (array[mid] == key) {
-        return mid;
-    }
+  if (array[mid] == key) {
+    return mid;
+  }
 
-    if (left <= right) {
-        if (array[mid] < key) {
-            return binary_search_recursive(array, mid + 1, right, key);
-        } else {
-            return binary_search_recursive(array, left, mid -1, key);
-        }
+  if (left <= right) {
+    if (array[mid] < key) {
+      return binary_search_recursive(array, mid + 1, right, key);
     } else {
-        return -1;
+      return binary_search_recursive(array, left, mid - 1, key);
     }
+  } else {
+    return -1;
+  }
 }
 
 void insertion_sort_with_binary_search(int array[], int sizearray) {
-    int key = 0, j, i, right, left, mid; 
+  int key = 0, j, i, right, left, mid;
 
-    for (j = 1; j < sizearray; j++) {
-        key = array[j];
-        right = j - 1;
-        left = -1;   
+  for (j = 1; j < sizearray; j++) {
+    key = array[j];
+    right = j - 1;
+    left = -1;
 
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (key > array[mid]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        mid = (left + right) / 2;
-
-        for (i = j - 1; i > mid; i--) {
-            array[i + 1] = array[i];
-        }
-
-        array[i + 1] = key;
-        
+    while (left < right) {
+      mid = (left + right) / 2;
+      if (key > array[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
+
+    mid = (left + right) / 2;
+
+    for (i = j - 1; i > mid; i--) {
+      array[i + 1] = array[i];
+    }
+
+    array[i + 1] = key;
+  }
 }
 
 bool sum_of_two_numbers(int array[], int sizearray, int result) {
-    int i, j;
+  int i, j;
 
-    for (i = 0; i < sizearray; i++) {
-        for (j = i + 1; j < sizearray; j++) {
-            if (array[i] + array[j] == result) {
-                return true;
-            }
-        }
+  for (i = 0; i < sizearray; i++) {
+    for (j = i + 1; j < sizearray; j++) {
+      if (array[i] + array[j] == result) {
+        return true;
+      }
     }
+  }
 
-    return false;
+  return false;
 }
 
 bool sum_of_two_numbers_v2(int array[], int sizearray, int result) {
-    int x = 0; 
-    merge_sort(array, 0, sizearray);
+  int x = 0;
+  merge_sort(array, 0, sizearray);
 
-    for (int i = 0; i < sizearray; i++) {
-        x = result - array[i];
+  for (int i = 0; i < sizearray; i++) {
+    x = result - array[i];
 
-        if (binary_search_recursive(array, 0, sizearray, x) != -1) {
-            return true;
-        }
+    if (binary_search_recursive(array, 0, sizearray, x) != -1) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 void bubble_sort(int array[], int sizearray) {
-    int i, j, aux;
+  int i, j, aux;
 
-    for (i = 0; i < sizearray; i++) {
-        for (j = i + 1; j < sizearray; j++) {
-            if (array[i] > array[j]) {
-                aux = array[i];
-                array[i] = array[j];
-                array[j] = aux;
-            }
-        }
+  for (i = 0; i < sizearray; i++) {
+    for (j = i + 1; j < sizearray; j++) {
+      if (array[i] > array[j]) {
+        aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+      }
     }
+  }
 }
 
 IntervalMaxSubarray max_subarray(int array[], int init_array, int size_array) {
-  IntervalMaxSubarray maxleftarray, maxacrossarray, maxrightarray; 
-  int mid = (init_array + size_array) / 2; 
+  IntervalMaxSubarray maxleftarray, maxacrossarray, maxrightarray;
+  int mid = (init_array + size_array) / 2;
   int sum = array[init_array];
   int end_interval = init_array;
 
@@ -277,9 +278,11 @@ IntervalMaxSubarray max_subarray(int array[], int init_array, int size_array) {
     maxrightarray = max_subarray(array, mid, size_array);
     maxacrossarray = max_middle_subarray(array, init_array, mid, size_array);
 
-    if (maxleftarray.sum > maxrightarray.sum && maxleftarray.sum > maxacrossarray.sum) {
+    if (maxleftarray.sum > maxrightarray.sum &&
+        maxleftarray.sum > maxacrossarray.sum) {
       return maxleftarray;
-    } else if (maxrightarray.sum > maxleftarray.sum && maxrightarray.sum > maxacrossarray.sum) {
+    } else if (maxrightarray.sum > maxleftarray.sum &&
+               maxrightarray.sum > maxacrossarray.sum) {
       return maxrightarray;
     } else {
       return maxacrossarray;
@@ -294,7 +297,8 @@ IntervalMaxSubarray max_subarray(int array[], int init_array, int size_array) {
   }
 }
 
-IntervalMaxSubarray max_middle_subarray(int array[], int init_array, int mid, int size_array) {
+IntervalMaxSubarray max_middle_subarray(int array[], int init_array, int mid,
+                                        int size_array) {
   IntervalMaxSubarray across_subarray;
   int sum = array[mid];
   int init_interval = mid, end_interval = mid;
@@ -306,12 +310,12 @@ IntervalMaxSubarray max_middle_subarray(int array[], int init_array, int mid, in
     if (sum > total) {
       total = sum;
       init_interval = i;
-    } 
+    }
   }
-  
-  sum = total; 
+
+  sum = total;
   for (int j = mid + 1; j < size_array; j++) {
-    sum += array[j]; 
+    sum += array[j];
 
     if (sum > total) {
       total = sum;
@@ -327,7 +331,7 @@ IntervalMaxSubarray max_middle_subarray(int array[], int init_array, int mid, in
 }
 
 IntervalMaxSubarray max_subarray_linear(int array[], int size_array) {
-  IntervalMaxSubarray max, previous; 
+  IntervalMaxSubarray max, previous;
   int sequence;
 
   max.init = 0;
@@ -343,7 +347,7 @@ IntervalMaxSubarray max_subarray_linear(int array[], int size_array) {
   }
 
   for (int i = 1; i < size_array; i++) {
-    sequence = array[i]; 
+    sequence = array[i];
 
     if (previous.sum + sequence < sequence) {
       previous.sum = sequence;
@@ -351,7 +355,7 @@ IntervalMaxSubarray max_subarray_linear(int array[], int size_array) {
       previous.end = i;
     } else {
       previous.sum += sequence;
-      previous.end = i; 
+      previous.end = i;
     }
 
     if (max.sum < previous.sum) {
@@ -364,14 +368,8 @@ IntervalMaxSubarray max_subarray_linear(int array[], int size_array) {
   return max;
 }
 
-HeapSort::HeapSort(int array*) {
-	array = array;
-};
+HeapSort::HeapSort(int *array) { array = array; };
 
-int HeapSort::left(int index) {
-	return index * 2 + 1;
-};
+int HeapSort::left(int index) { return index * 2 + 1; };
 
-int HeapSort::right(int index) {
-	return index * 2 + 2; 
-};
+int HeapSort::right(int index) { return index * 2 + 2; };
--- algorithms/algorithms.h
+++ algorithms/algorithms.h
@@ -9,31 +9,37 @@ struct IntervalMaxSubarray {
   int sum;
 };
 
-void print_vetor(int array[], int sizearray); //func aux
-void insertion_sort(int array[], int sizearray); //exercise 2.1-2
-int linear_search(int array[], int sizearray, int key); //exercise 2.1-3
-void plus_bit(int parcela_a[], int parcela_b[]); //plus num bit with 2 cases
-void insertion_sort_with_search_minus_value(int array[], int sizearray); //exercise 2.2-2
-void merge_sort(int array[], int init_array, int sizearray); //exercise 
-void insertion_sort_recursive(int array[], int sizearray); //exercise 2.3-3 
-int binary_search(int array[], int left, int right, int key); //exercise 2.3-5
-int binary_search_recursive(int array[], int left, int right, int key); //exercise 2.3-5
-void insertion_sort_with_binary_search(int array[], int sizearray); //exercise 2.3-6
-bool sum_of_two_numbers(int array[], int sizearray, int result); //exercise 2.3-7
-bool sum_of_two_numbers_v2(int array[], int sizearray, int result); //exercise 2.3-7
-void bubble_sort(int array[], int sizearray); 
+void print_vetor(int array[], int sizearray);           // func aux
+void insertion_sort(int array[], int sizearray);        // exercise 2.1-2
+int linear_search(int array[], int sizearray, int key); // exercise 2.1-3
+void plus_bit(int parcela_a[], int parcela_b[]); // plus num bit with 2 cases
+void insertion_sort_with_search_minus_value(int array[],
+                                            int sizearray);   // exercise 2.2-2
+void merge_sort(int array[], int init_array, int sizearray);  // exercise
+void insertion_sort_recursive(int array[], int sizearray);    // exercise 2.3-3
+int binary_search(int array[], int left, int right, int key); // exercise 2.3-5
+int binary_search_recursive(int array[], int left, int right,
+                            int key); // exercise 2.3-5
+void insertion_sort_with_binary_search(int array[],
+                                       int sizearray); // exercise 2.3-6
+bool sum_of_two_numbers(int array[], int sizearray,
+                        int result); // exercise 2.3-7
+bool sum_of_two_numbers_v2(int array[], int sizearray,
+                           int result); // exercise 2.3-7
+void bubble_sort(int array[], int sizearray);
 IntervalMaxSubarray max_subarray(int array[], int init_array, int size_array);
-IntervalMaxSubarray max_middle_subarray(int array[], int init_array, int mid, int size_array);
+IntervalMaxSubarray max_middle_subarray(int array[], int init_array, int mid,
+                                        int size_array);
 IntervalMaxSubarray max_subarray_linear(int array[], int size_array);
 
 class HeapSort {
-	int array*;
+  int *array;
 
-	public:
-		HeapSort(int array*);
-		int left(int index);
-		int right(int index);
-		void max_heapify(int array*, int index);
+public:
+  HeapSort(int *array);
+  int left(int index);
+  int right(int index);
+  void max_heapify(int *array, int index);
 };
 
-#endif 
+#endif
